fe93efc0e7281e652cdcf73216ad0ded131d0853
blackzarifa <rafaelirades@gmail.com>
2025-04-17 23:11:43 -0300

    initial REPL steps

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+
+		rawText := scanner.Text()
+		cleanText := cleanInput(rawText)
+
+		fmt.Printf("Your command was: %s\n", cleanText[0])
+	}
 }
 
 func cleanInput(text string) []string {
