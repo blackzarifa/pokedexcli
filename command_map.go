8baf4afcddd04d454efb9fb1eb55a1155fed163b
blackzarifa <rafaelirades@gmail.com>
2025-04-20 00:50:49 -0300

    refac command_map functions

--- command_map.go
+++ command_map.go
@@ -1,75 +1,41 @@
 package main
 
 import (
-	"encoding/json"
+	"errors"
 	"fmt"
-	"net/http"
 )
-
-type LocationArea struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-
-type LocationAreaResponse struct {
-	Previous string         `json:"previous"`
-	Next     string         `json:"next"`
-	Results  []LocationArea `json:"results"`
-}
-
-var (
-	previousURL string
-	nextURL     string
-)
-
-func fetchLocationAreas(url string) ([]LocationArea, error) {
-	if url == "" {
-		url = "https://pokeapi.co/api/v2/location-area"
-	}
-
-	res, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	var locationAreas LocationAreaResponse
-	err = json.NewDecoder(res.Body).Decode(&locationAreas)
-	if err != nil {
-		return nil, err
-	}
-
-	previousURL = locationAreas.Previous
-	nextURL = locationAreas.Next
-
-	return locationAreas.Results, nil
-}
 
 func commandMap(cfg *config) error {
-	locationAreas, err := fetchLocationAreas(nextURL)
+	locationRes, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsURL)
 	if err != nil {
 		return err
 	}
 
-	for _, area := range locationAreas {
-		fmt.Println(area.Name)
+	cfg.nextLocationsURL = locationRes.Next
+	cfg.prevLocationsURL = locationRes.Previous
+
+	for _, location := range locationRes.Results {
+		fmt.Println(location.Name)
 	}
 
 	return nil
 }
 
 func commandMapB(cfg *config) error {
-	if previousURL == "" {
-		return fmt.Errorf("you're on the first page")
+	if cfg.prevLocationsURL == nil {
+		return errors.New("you're on the first page")
 	}
 
-	locationAreas, err := fetchLocationAreas(previousURL)
+	locationRes, err := cfg.pokeapiClient.ListLocations(cfg.prevLocationsURL)
 	if err != nil {
 		return err
 	}
 
-	for _, area := range locationAreas {
-		fmt.Println(area.Name)
+	cfg.nextLocationsURL = locationRes.Next
+	cfg.prevLocationsURL = locationRes.Previous
+
+	for _, location := range locationRes.Results {
+		fmt.Println(location.Name)
 	}
 
 	return nil
