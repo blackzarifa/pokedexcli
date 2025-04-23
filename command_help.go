c6497a8a2ab93551e0d52a7148ec4fc87e87b628
blackzarifa <rafaelirades@gmail.com>
2025-04-23 18:46:02 -0300

    fix args types

--- command_help.go
+++ command_help.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func commandHelp(cfg *config, args ...strings) error {
+func commandHelp(cfg *config, args ...string) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Print("Usage:\n\n")
 
--- command_map.go
+++ command_map.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func commandMap(cfg *config, args ...strings) error {
+func commandMap(cfg *config, args ...string) error {
 	locationRes, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsURL)
 	if err != nil {
 		return err
@@ -21,7 +21,7 @@ func commandMap(cfg *config, args ...strings) error {
 	return nil
 }
 
-func commandMapB(cfg *config, args ...strings) error {
+func commandMapB(cfg *config, args ...string) error {
 	if cfg.prevLocationsURL == nil {
 		return errors.New("you're on the first page")
 	}
