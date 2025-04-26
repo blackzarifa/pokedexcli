fbb057d5cb041089d17e135c2c8210a49c4786e6
blackzarifa <rafaelirades@gmail.com>
2025-04-26 00:35:36 -0300

    adjustments

--- command_catch.go
+++ command_catch.go
@@ -11,10 +11,8 @@ func commandCatch(cfg *config, args ...string) error {
 	if len(args) != 1 {
 		return errors.New("you must provide a pokemon name")
 	}
+
 	pokemonName := args[0]
-
-	fmt.Printf("Throwing a Pokeball at %s...\n", pokemonName)
-
 	pokemon, err := cfg.pokeapiClient.GetPokemon(pokemonName)
 	if err != nil {
 		return err
@@ -29,6 +27,7 @@ func commandCatch(cfg *config, args ...string) error {
 	chance := 50.0 / float64(baseExp)
 	catch := rng.Float64() < chance
 
+	fmt.Printf("Throwing a Pokeball at %s...\n", pokemonName)
 	if catch {
 		fmt.Println(pokemonName + " was caught!")
 		cfg.pokedex[pokemonName] = pokemon
