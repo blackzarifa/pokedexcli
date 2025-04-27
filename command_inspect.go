c428e3d28a29b9a58290e1aff496a41a5a68d016
blackzarifa <rafaelirades@gmail.com>
2025-04-27 15:18:22 -0300

    feat inspect command

--- command_inspect.go
+++ command_inspect.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 func commandInspect(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("you must provide a pokemon name")
+	}
+
+	pokemonName := args[0]
+	pokemon, ok := cfg.pokedex[pokemonName]
+	if !ok {
+		return errors.New("you have not caught that pokemon")
+	}
+
+	fmt.Printf("Name: %v\n", pokemon.Name)
+	fmt.Printf("Height: %v\n", pokemon.Height)
+	fmt.Printf("Weight: %v\n", pokemon.Weight)
+
+	fmt.Println("Types:")
+	for _, t := range pokemon.Types {
+		fmt.Printf("  - %v\n", t.Type.Name)
+	}
+
+	fmt.Println("Stats:")
+	for _, s := range pokemon.Stats {
+		fmt.Printf("  -%v: %v\n", s.Stat.Name, s.BaseStat)
+	}
+
 	return nil
 }
