a653c0813cea4f0368b60c3feaad9e16b2a2d921
blackzarifa <rafaelirades@gmail.com>
2025-04-29 00:01:13 -0300

    feat pokedex command

--- command_pokedex.go
+++ command_pokedex.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 func commandPokedex(cfg *config, args ...string) error {
+	if len(cfg.pokedex) < 1 {
+		return errors.New("you have not caught any pokemon yet")
+	}
+
+	fmt.Println("Your Pokedex:")
+	for _, p := range cfg.pokedex {
+		fmt.Println("  - " + p.Name)
+	}
 
 	return nil
 }
